9a46ed897a823f7e2e0358761490b6f2c5b1202a
bat0slurp <85578682+Lucas-De-Martin-Bianco@users.noreply.github.com>
2023-12-22 20:58:07 +0100

    Products
    Produkte hinzugefügt. Andere Elemente angepasst, damit sie zu den Produkten passen.

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -13,13 +13,15 @@ export default {
 <template>
   <div class="card mb-3" style="max-width: 540px">
     <div class="row no-gutters">
-      <div class="col-md-4">
+      <div class="col-md-4 d-flex align-items-center">
         <img :src="data.imageUrl" class="card-img" alt="..." />
       </div>
       <div class="col-md-8">
         <div class="card-body">
           <h5 class="card-title">{{ data.productName }}</h5>
-          <Rating :initalRating="data.rating" />
+          <Rating :initalRating="data.rating"/>
+          <p class="card-text"></p>
+          <p class="card-text">{{ data.price }}</p>
           <p class="card-text">{{ data.description }}</p>
           <p class="card-text">
             <small class="text-muted">{{ data.releasedDate }}</small>
